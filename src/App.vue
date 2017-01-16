b0a05af415afff9fc705aaae41fd5e328224f23b
David Hanaford <davehanaford@gmail.com>
2017-01-16 18:32:39 -0500

    first pass at design, copy, branding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
     <div id="app">
-        <div v-if="state.showSplash" class="splash-screen">
-          <div class="instruction-dialog">
-            <h2>Welcome to the Random Question Generator!</h2>
-            <p>This game is meant to inspire conversation and can also be a great way to break the ice. This tool can be used solo, i.e (if you are on a date and need a quick way to start conversation), or played with any number of people. There are 2 basic rules.</p>
-            <ol>
-              <li>One person asks and generates the questions</li>
-              <li>Each person answers but may feel free to just pass if they don’t feel like answering</li>
-            </ol>
-            <p>As long as each person has their own time to have a turn to give there answer without interruption.</p>
+      <div class="container-main">
+        <div v-if="state.showSplash" class="flex">
+          <div class="content-wrapper instruction-dialog">
+              <p><b>Generate some randomized question based on a category of your choosing or just generate a completely random question.</b> People love having good quality conversation and laughter. People also like staring at the computer screens and smart phones all day long. Why not combine both!</p>
+              <p>There are no rules. As long as everyone gets an uninterrupted chance to answer the question, then this game can be rewarding and fun way to open up and learn about one and ither.</p>
             <form class="form-inline category">
-              <p>Pick a category below to get started:</p>
+              <p><b>Pick a category below to get started!</b></p>
               <select :value="state.chosenCategory" v-model="state.chosenCategory" class="form-control">
                 <option v-for="category in content.categories">{{category}}</option>
               </select>
             </form>
             <div class="btn-container">
-              <button @click="startGame()" class="btn btn-pimary">Get Started</button>
+              <button @click="startGame()" class="btn btn-pimary">Generate!</button>
             </div>
           </div>
         </div>
-        <div class="container-main">
-            <div class="flex">
-              <p>{{ state.singleQuestion }}</p>
-              <button class="btn btn-primary" @click="nextQuestion()">Next Question</button>
-              <label>Change Category</label>
-              <select @change="changeCategory()" v-model="state.chosenCategory" class="form-control">
-                <option v-for="category in content.categories">{{category}}</option>
-              </select>
+        <div v-if="state.showSplash === false" class="flex">
+              <div class="content-wrapper">
+                <div class="question">
+                  <p>{{ state.singleQuestion }}</p>
+                </div>
+                <button class="btn btn-primary" @click="nextQuestion()">Generate Question</button>
+                <br />
+                <br />
+                <label>Change Category</label>
+                <select @change="changeCategory()" v-model="state.chosenCategory" class="form-control form-control-sm category">
+                  <option v-for="category in content.categories">{{category}}</option>
+                </select>
+                <div class="category-chooser">
+                  <p>Current category: <span>{{state.currentCategory}}</span></p>
+                </div>
             </div>
         </div>
     </div>
@@ -45,7 +48,8 @@ export default {
                 'categorized': [],
                 'singleQuestion': '',
                 'chosenCategory': 'Select a category',
-                'changedCategory': 'Change Category'
+                'changedCategory': 'Change Category',
+                'currentCategory': ''
             },
             'content': {
                 'categories': ['icebreaker', 'deep', 'hypothetical', 'relationships']
@@ -75,22 +79,18 @@ export default {
         'startGame': function () {
             this.state.categorized = _.filter(this.state.questions, { 'category': this.state.chosenCategory })
             this.$set('state.categorized', this.state.categorized)
-            this.state.singleQuestion = this.state.categorized[0].body
+            this.state.singleQuestion = this.state.categorized.body
+            this.state.currentCategory = this.state.categorized[0].category
             this.state.showSplash = false
         },
         'changeCategory': function () {
             this.state.chosenCategory = _.filter(this.state.questions, { 'category': this.state.chosenCategory })
             this.$set('state.categorized', this.state.chosenCategory)
-            this.state.singleQuestion = this.state.categorized[0].body
+            this.state.singleQuestion = this.state.categorized.body
+            this.state.currentCategory = this.state.categorized[0].category
         },
         'nextQuestion': function () {
             this.state.singleQuestion = this.state.categorized.shift().body
-        }
-    },
-    'computed': {
-        'findCategories': function () {
-            var test = _.find(this.state.questions, { 'category': this.state.chosenCategory })
-            return test
         }
     }
 }
@@ -104,7 +104,7 @@ html {
 }
 
 body {
-    background-color: #42413d;
+    background-color: #F9CA00;
     color: #b2afac;
     font-size: 62.5%;
 }
@@ -113,9 +113,45 @@ h1 {
     font-size: 2.5rem;
 }
 
+h2 {
+  text-align: center;
+  font-size: 2rem;
+  color: black;
+  font-weight: bold;
+}
+
+h3, h4 {
+  color: black;
+}
+
 p {
     font-size: 1rem;
-    color: white;
+    color: black;
+}
+
+.question {
+  min-height: 90px;
+  p {
+    color: black;
+    font-size: 1.9rem;
+    font-weight: bold;
+  }
+}
+.category-chooser {
+  text-transform: capitalize;
+  position: relative;
+  p {
+    color: #444;
+    text-transform: uppercase;
+    font-size: .88rem;
+    position: absolute;
+    top: 87px;
+    left: 12px;
+  }
+  span {
+    font-weight: bold;
+    color: #6BAB03;
+  }
 }
 
 .container-main {
@@ -123,20 +159,35 @@ p {
   justify-content: center;
   width: 100%;
   height: 100vh;
+  padding-top: 5%;
 }
 
 .flex {
   text-align: center;
-  padding: 2em;
   margin: auto;
-  width: 60%;
-  min-width: 300px;
+  width: 100%;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background: transparent url(/static/hero-bg.svg) no-repeat;
+  background-size: contain;
+  height: 700px;
+  background-position-x: center;
+}
+
+.content-wrapper {
+  margin-top: 159px;
+  width: 41rem;
+  margin: auto;
+  padding-top: 9rem;
+  padding-right: 5rem;
 }
 
 .btn-primary {
-    background-color: #ffce2b;
+    background-color: #24aadd;
     color: white;
-    border: 1px solid #e6b000;
+    border: 1px solid #0D8BBB;
+    border-radius: 0px;
 }
 
 .btn-primary:hover,
@@ -144,9 +195,9 @@ p {
 .btn-primary:focus,
 .btn-primary:visited,
 .btn-primary:link {
-    background-color: #b38900;
+    background-color: #56D1FF;
     color: white;
-    border: 1px solid #e6b000;
+    border: 1px solid #0D8BBB;
 }
 
 .splash-screen {
@@ -155,13 +206,20 @@ p {
   justify-content: center;
   width: 100%;
   height: 100vh;
-  background-color: black;
+  background-color: #F9CA00;
   z-index: 1;
 }
 
 ol li {
   font-size: .8rem;
   line-height: 1.6rem;
+  color: black;
+  text-align: left;
+}
+
+select.category {
+  width: 295px;
+  margin: auto;
 }
 
 .btn-container {
@@ -169,26 +227,22 @@ ol li {
   justify-content: center;
   button {
     margin-top: 1em;
+    background-color: #24aadd;
+    color: white;
+    border: 1px solid #0D8BBB;
   }
 }
 
 .instruction-dialog {
-  padding: 2em;
-  margin: auto;
-  width: 60%;
-  min-width: 300px;
-  height: 50em;
-  background-color: #eee;
-  color: #444;
-
-  h2 {
-    text-align: center;
-    font-size: 2rem;
-  }
-
+  width: 44rem;
+  padding: 9rem 7rem 7rem 0;
   p {
-    color: #444;
+    color: black;
   }
+}
+
+label {
+  color: black;
 }
 
 form.category {
